50dc93d52eb82f9d53aa769e9d75f073033c7152
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2025-06-09 19:07:47 +0200

    create RSA keys in runtime

--- src/token.js
+++ src/token.js
@@ -2,7 +2,44 @@ const jwt = require("jsonwebtoken")
 const fs = require("fs")
 const path = require("path")
 
-const privateKey = fs.readFileSync(path.join(__dirname, "./keys/private.key"))
+const logger = require("./logger")
+
+const { generateKeyPairSync } = require("crypto")
+
+const keyPath = path.join(__dirname, "../secrets/private_key.pem")
+
+let privateKey
+
+function CheckForKey() {
+	if (fs.existsSync(keyPath)) {
+		privateKey = fs.readFileSync(keyPath, "utf8")
+
+		logger.log("Loaded existing RSA private key")
+	} else {
+		const { privateKey: genPrivKey, publicKey: genPubKey } =
+			generateKeyPairSync("rsa", {
+				modulusLength: 2048,
+				publicKeyEncoding: {
+					type: "spki",
+					format: "pem",
+				},
+				privateKeyEncoding: {
+					type: "pkcs8",
+					format: "pem",
+				},
+			})
+
+		fs.mkdirSync(path.dirname(keyPath), { recursive: true })
+		fs.writeFileSync(keyPath, genPrivKey)
+		fs.writeFileSync(
+			path.join(__dirname, "../secrets/public_key.pem"),
+			genPubKey
+		)
+		privateKey = genPrivKey
+
+		logger.log("Generated new RSA key pair")
+	}
+}
 
 function SignToken(payload) {
 	const options = {
@@ -17,5 +54,8 @@ function DecodeToken(idToken) {
 	return jwt.decode(idToken)
 }
 
+CheckForKey()
+
 exports.SignToken = SignToken
 exports.DecodeToken = DecodeToken
+exports.CheckForKey = CheckForKey
